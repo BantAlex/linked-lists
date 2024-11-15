bfae2dd01e6d80aa48134746612b90fadef8233d
BantAlex <bantalex3@gmail.com>
2024-11-15 16:09:27 +0200

    Rearange files

---
+++ lib/linked_list.rb
@@ -0,0 +1,48 @@
+class LinkedList #Whole List
+
+  def initialize
+    head = nil
+    tail = nil
+    seize = 0
+  end
+
+  def append(value) #Add a node containing @value to the start
+
+  end
+
+  def prepend(value) #Add a node containing @value to the end
+
+  end
+
+  def size #Return the total number of nodes in the list
+
+  end
+
+  def head #Return the first node on the list
+
+  end
+
+  def tail #Return the last node on the list
+
+  end
+
+  def at(index) #Return the node at index
+
+  end
+
+  def pop #Remove the last element from the list
+
+  end
+
+  def contains?(value) #Return true if value is in the list
+
+  end
+
+  def find(value) #Return index of value and nil if it's not existant
+
+  end
+
+  def to_s #Represent LinkedList objects as strings
+
+  end
+end
---
+++ lib/node.rb
@@ -0,0 +1,7 @@
+class Node
+  #2 Instance vars: #value, #next_node
+  def initialize
+    @value = nil
+    @next_node = nil
+  end
+end
